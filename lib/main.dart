38319e3159d35d0b63ecc434159a85ebec834c6f
Kaio Goes <kaiogoes1999@gmail.com>
2026-04-25 15:19:44 -0300

    Feature create divida Aqui

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:divida_aqui/pages/dashboard/dashboard_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dívida Aqui',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const DashboardPage(),
+    );
+  }
+}
